d92d10648bb2f9134c21fa781fa72db0a26f8104
Chippy Sebastian <chippysabu511@gmail.com>
2024-03-08 19:44:37 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// $('.arrival-tab-navs').click(function () {
+//     var tab_id = $(this).attr('data-tab');
+  
+//     $('.arrival-tab-navs').removeClass('active');
+//     $('.arrival-tab-content').removeClass('active');
+  
+//     $('.arrival-tab-navs[data-tab=' + tab_id + ']').addClass('active');
+//     $("#" + tab_id).addClass('active');
+  
+// });
+
+
+
+const hampMenu = document.querySelector(".hamburger-menu-design-icon");
+const navbar = document.querySelector(".header-nav");
+const bodyTag = document.querySelector("body");
+hampMenu.addEventListener("click", () => {
+  hampMenu.classList.toggle("open");
+  navbar.classList.toggle("open");
+  bodyTag.classList.toggle("o-hidden");
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(document);
+  let newArrivalButton=document.querySelectorAll(".arrival-tab-navs");
+  console.log(newArrivalButton);
+  
+  for(let i=0; i<newArrivalButton.length; i++){
+    newArrivalButton[i].addEventListener("click",navToggleBar)
+  }
+});
+
+function navToggleBar(){
+  console.log(this.dataset.tab);
+  let tabId = this.dataset.tab;
+  let tabContentClass = this.dataset.tabContentClass;
+  console.log(tabContentClass);
+  console.log(document.querySelector("arrival-tab-content"))
+  this.classList.remove("active");
+}
